6988e9a1c530a72741a9a3ea2d337370b8ae4365
ayshasanyang <isasanyang1986@yahoo.co.uk>
2025-01-25 21:21:00 -0600

    added style.css

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    const voteButton = document.querySelector('.vote-button');
+
+    voteButton.addEventListener('click', () => {
+      alert('Thanks for voting! 🎉 Comment below which spinner you liked best!');
+    });
